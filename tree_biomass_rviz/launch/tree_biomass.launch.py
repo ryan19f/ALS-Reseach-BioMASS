0fa5b3d70e743be308bdb6ee34d1333f4be03c84
Rhutvik-pachghare1999 <rpachgha@asu.edu>
2025-05-11 21:19:20 -0700

    Update tree_biomass.launch.py

--- tree_biomass_rviz/launch/tree_biomass.launch.py
+++ tree_biomass_rviz/launch/tree_biomass.launch.py
@@ -1,12 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+import os
 
 def generate_launch_description():
     return LaunchDescription([
         Node(
             package='tree_biomass_rviz',
-            executable='biomass_node',
-            name='biomass_node',
+            executable='biomass_visualizer_node',
+            name='biomass_visualizer_node',
+            output='screen'
+        ),
+        ExecuteProcess(
+            cmd=['rviz2', '-d', os.path.join(
+                os.getenv('HOME'),
+                'space_robotics_project/ros2_ws/src/tree_biomass_rviz/config/biomass_view.rviz'
+            )],
             output='screen'
         )
     ])
