9cbb64c4b2e12f5867e320e5e6e813f60187fef7
Rhutvik-pachghare1999 <rpachgha@asu.edu>
2025-05-11 21:22:22 -0700

    Update setup.py

--- tree_biomass_rviz/setup.py
+++ tree_biomass_rviz/setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup
-import os
-from glob import glob
 
 package_name = 'tree_biomass_rviz'
 
@@ -9,21 +7,23 @@ setup(
     version='0.1.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        ('share/ament_index/resource_index/packages', ['resource/tree_biomass_rviz']),
+        ('share/tree_biomass_rviz', ['package.xml']),
+        ('share/tree_biomass_rviz/launch', ['launch/biomass_visualizer.launch.py']),
+        ('share/tree_biomass_rviz/config', ['config/biomass_view.rviz']),  # Optional if config exists
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='rhutvik',
     maintainer_email='rhutvik@example.com',
-    description='Tree biomass estimation and RViz2 visualization using ALS point cloud',
-    license='MIT',
+    description='Tree biomass estimation and visualization in ROS 2',
+    license='TODO',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'biomass_node = tree_biomass_rviz.biomass_node:main',
+            'biomass_visualizer_node = tree_biomass_rviz.biomass_visualizer_node:main',
         ],
     },
-)
 
+)
